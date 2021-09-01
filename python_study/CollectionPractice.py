280714461ed848004439bad7f5dd164c86c02a31
Minjae <joshuara7235@spiderkim.com>
2021-09-01 16:21:08 +0900

    python list study

--- python_study/CollectionPractice.py
+++ python_study/CollectionPractice.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #List
 
 """
@@ -19,6 +20,40 @@ letter = a[0]
 print(letter)
 for element in a:
     print(element) # 수정된 element 출력
+
+testList = [1,3,5,7,9,11,13,15,17]
+a = testList[0:4] #1, 3, 5, 7
+print(a)
+b = testList[:3] #1, 3, 5
+print(b)
+c = testList[5:] # 11, 13, 15, 17
+print(c)
+
+print("List 추가, 수정, 삭제")
+testList1 = ["AB", 123, True]
+#추가
+testList1.append("Plus")
+#수정
+testList1[1] = 456
+#삭제
+del testList1[2]
+
+for element in testList1:
+    print(element)
+
+print("list 병합 및 반복")
+
+listA = [1, 2, 3]
+listB = [4, 5, 6]
+result = listA + listB
+print(result)
+
+resultMul = listA * 3
+print(resultMul)
+
+print
+
+
 
 
 
